ea3b08d7357fadd908c9a37d4f5a9a2abbab5830
VK00329626 <VK00329626@INHYIZLP04934.TechMahindra.com>
2020-06-08 23:17:43 -0400

    Even numbers

---
+++ corejava/src/class1/Evennumber.java
@@ -0,0 +1,39 @@
+package class1;
+
+public class Evennumber {
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=100;
+		int a = 0;
+		
+		
+		for(int i=1;i<=n;i++){
+			
+		a = i;
+		if(a%2 == 0){
+			
+			System.out.println(a);
+			
+		}
+			
+				
+				
+				
+				
+		
+			
+			
+			
+		}
+			
+		
+		
+		
+
+	}
+
+}
